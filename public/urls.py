7e0b5dbfc304b52e62cccc7297774c1695ff0909
Carlos Alba <hola@carlosalba.net>
2025-07-12 10:39:49 +0300

    Implementación de nuevas vistas y URLs para las secciones "Acerca de", "Servicios" y "Testimonios". Se han añadido plantillas correspondientes y se han actualizado los estilos CSS para mejorar la presentación visual. También se han incluido nuevas imágenes en la carpeta de recursos.

--- public/urls.py
+++ public/urls.py
@@ -5,4 +5,7 @@ app_name = 'public'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('acerca-de/', views.about, name='about'),
+    path('servicios/', views.services, name='services'),
+    path('testimonios/', views.testimonials, name='testimonials'),
 ]
--- public/views.py
+++ public/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'public/index.html')
+
+
+def about(request):
+    return render(request, 'public/about.html')
+
+
+def services(request):
+    return render(request, 'public/services.html')
+
+
+def testimonials(request):
+    return render(request, 'public/testimonials.html')
+
+
+def contact(request):
+    return render(request, 'public/contact.html')
