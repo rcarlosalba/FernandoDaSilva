0d4de6134411272d9de5819d398c864d33a86f9c
Carlos Alba <hola@carlosalba.net>
2025-07-21 20:08:02 +0300

    Actualización de las URLs en la aplicación de dashboard para la gestión de newsletters. Se han modificado las rutas para incluir el prefijo 'newsletter/' y se han corregido las referencias en las plantillas para asegurar la correcta navegación. Estas mejoras facilitan la organización y el acceso a las funcionalidades relacionadas con newsletters.

--- dashboard/urls/newsletter_urls.py
+++ dashboard/urls/newsletter_urls.py
@@ -5,10 +5,12 @@ from dashboard.views.newsletter_views import (
 )
 
 urlpatterns = [
-    path('', newsletter_list, name='list_newsletter'),
-    path('create/', newsletter_create, name='create_newsletter'),
-    path('<int:pk>/edit/', newsletter_edit, name='edit_newsletter'),
-    path('<int:pk>/delete/', newsletter_delete, name='delete_newsletter'),
-    path('<int:pk>/preview/', newsletter_preview, name='preview_newsletter'),
-    path('<int:pk>/send/', newsletter_send, name='send_newsletter'),
+    path('newsletter/', newsletter_list, name='list_newsletter'),
+    path('newsletter/create/', newsletter_create, name='create_newsletter'),
+    path('newsletter/<int:pk>/edit/', newsletter_edit, name='edit_newsletter'),
+    path('newsletter/<int:pk>/delete/',
+         newsletter_delete, name='delete_newsletter'),
+    path('newsletter/<int:pk>/preview/',
+         newsletter_preview, name='preview_newsletter'),
+    path('newsletter/<int:pk>/send/', newsletter_send, name='send_newsletter'),
 ]
