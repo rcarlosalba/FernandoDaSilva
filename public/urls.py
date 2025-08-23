201452d9490b634ce29ca2e292c91f3d3d00db4a
Carlos Alba <hola@carlosalba.net>
2025-08-23 16:58:22 +0300

    fixing contact url

--- public/urls.py
+++ public/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('acerca-de/', views.about, name='about'),
     path('servicios/', views.services, name='services'),
     path('testimonios/', views.testimonials, name='testimonials'),
+    path('contacto/', views.contact, name='contact'),
 ]
